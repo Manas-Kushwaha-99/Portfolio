01c85e508b5d523694c190d35ee3820829e216aa
Manas Kushwaha <manaskushwaha99@gmail.com>
2026-02-23 20:08:59 +0530

    Refactor personalInfo and event listeners

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function showCertificates() {
         <a href="https://codesignal.com/learn/certificates/cmgwx47p10060l704oytu0whu/course-paths/21" target="_blank">Introduction to Programming with Python — Code Signal</a>
       </li>
       <li>
-        <a href="https://certx.in/certificate/0270772f-3809-4400-b29b-1e1c61cd09971079573" target="_blank">AI Tools Workshop — be10x</a>
+        <a href="https://drive.google.com/file/d/19kpfjw1sRZI05YMUy2CSuvq3KCsyRfpJ/view?usp=sharing" target="_blank">AI Tools Workshop — be10x</a>
       </li>
     </ul>
   `;
@@ -119,6 +119,7 @@ function toggleTheme() {
     document.querySelector('#themeToggle i').className = isDark ? 'bi bi-sun' : 'bi bi-moon';
 
 }
+
 
 
 
